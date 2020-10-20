129e33d81a52a0dec3d1c9daabee470b47b6d676
Sowmya Billakanti <sowmya.billakanti8@gmail.com>
2020-10-20 15:45:16 -0700

    Feature tast 1

---
+++ data/seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO books (title, author, isbn, image_url, description, bookshelf) VALUES (
+  'Animal Farm',
+  'George Orwell',
+  'ISBN_13 9780573605383',
+  'http://books.google.com/books/content?id=T0L2nUOPUqAC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'Animal Farm is a fable with a sting. Millions of words have been written about the threat of Totalitarianism, but it remained to the late George Orwell, farsighted British author of the brilliant and frightening 1984, to expose the Russian experiment for what it really is: an idealist''s dream, converted by realists into a nightmare. In Nelson Bond''s simple staged dramatic reading version of this timely allegory you will meet beasts whose prototypes have dominated news headlines for a half hundred fearful years. Opening on a note of joyous triumph for the creatures who have emancipated themselves from the cruel mastery of a human owner, the reading mounts inexorably to a climax of disillusionment in which the other animals discover themselves now subject to the rule of even more ruthless autocrats: the greedy, cunning pigs.',
+  'Satire'
+);
+INSERT INTO books (title, author, isbn, image_url, description, bookshelf) VALUES (
+  'The Great Gatsby',
+  'Francis Scott Fitzgerald',
+  'ISBN_13 9786050418156',
+  'http://books.google.com/books/content?id=meVxCwAAQBAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api',
+  'The main events of the novel take place in the summer of 1922. Nick Carraway, a Yale graduate and World War I veteran from the Midwest – who serves as the novel''s narrator – takes a job in New York as a bond salesman. He rents a small house on Long Island, in the (fictional) village of West Egg, next door to the lavish mansion of Jay Gatsby, a mysterious millionaire who holds extravagant parties but does not participate in them. Nick drives around the bay to East Egg for dinner at the home of his cousin, Daisy Fay Buchanan, and her husband, Tom, a college acquaintance of Nick''s. They introduce Nick to Jordan Baker, an attractive, cynical young golfer with whom Nick begins a romantic relationship. She reveals to Nick that Tom has a mistress, Myrtle Wilson, who lives in the "valley of ashes": an industrial dumping ground between West Egg and New York City. Not long after this revelation, Nick travels to New York City with Tom and Myrtle to an apartment they keep for their affair. At the apartment, a vulgar and bizarre party takes place. It ends with Tom breaking Myrtle''s nose after she annoys him by saying Daisy''s name several times.',
+  'Classics'
+);
+
